86c5830930171e005bae8d47a1d0d6d1eaefb491
ADMIN\Admin <vinhha2701@gmail.com>
2022-02-23 10:16:53 +0700

    first commit

---
+++ src/main/java/vn/gpay/jitin/core/stockout/IStockOutDService.java
@@ -0,0 +1,32 @@
+package vn.gpay.jitin.core.stockout;
+
+import java.util.List;
+
+import vn.gpay.jitin.core.base.Operations;
+
+public interface IStockOutDService extends Operations<StockOutD>{
+	List<StockOutD> getBystockOutId(Long stockoutid_link);
+	
+	List<Long> getStockoutIdBySkuId(Long skuid_link);
+
+	List<StockOutD> findBy_PContractAndSku(Long pcontractid_link, Long pcontract_poid_link, Long porderid_link,Long stockid_link, Long skuid_link);
+
+	void recal_Totalcheck(Long stockoutdid_link);
+
+	List<StockOutD> to_production_m_findBy_PContractAndSku(Long pcontractid_link, Long pcontract_poid_link, Long porderid_link,
+			Long stockid_link, Long skuid_link);
+
+	List<StockOutD> to_vendor_p_findBy_PContractAndSku(Long pcontractid_link, Long pcontract_poid_link, Long porderid_link,
+			Long stockid_link, Long skuid_link);
+
+//	List<StockOutD> p_findBy_PContractAndSku(Long pcontractid_link, Long pcontract_poid_link, Long porderid_link,
+//			Long stockid_link, Long skuid_link);
+	
+	List<StockOutD> getBy_MaNpl_StockoutId(String maNpl, Long stockoutid_link);
+
+	List<StockOutD> to_vendor_p_findBy_PContract(Long pcontractid_link, Long pcontract_poid_link, Long porderid_link,
+			Long stockid_link);
+
+	List<StockOutD> to_production_m_findBy_PContract(Long pcontractid_link, Long pcontract_poid_link,
+			Long porderid_link, Long stockid_link);
+}
